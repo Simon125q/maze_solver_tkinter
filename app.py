7b56ea7a9ea20f69418c991b5b3d6819bf333037
Simon125q <106029626+Simon125q@users.noreply.github.com>
2024-08-01 22:17:05 +0200

    added method to connect centers of 2 cells

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ from shapes import Point
 def main() -> None:
     win = Window(800, 600)
     c1 = Cell(Point(30, 10), Point(50, 100), win)
-    c2 = Cell(Point(100, 120), Point(140, 160), win)
+    c2 = Cell(Point(400, 120), Point(440, 160), win)
     c3 = Cell(Point(320, 360), Point(360, 400), win)
-    c3.walls.top = False
-    c3.walls.bottom = False
+    c1.walls.top = False
+    c2.walls.bottom = False
     c3.walls.left = False
     c3.walls.right = False
     c1.draw("black")
     c2.draw("black")
     c3.draw("black")
+    c1.draw_move(c3)
+    c2.draw_move(c1, True)
     win.wait_for_close()
 
 if __name__ == "__main__":
--- cell.py
+++ cell.py
@@ -29,3 +29,17 @@ class Cell:
         if self.walls.left:
             left_line = Line(self._p1, Point(self._p1.x, self._p2.y))
             self._win.draw_line(left_line, color)
+
+    def draw_move(self, to_cell: 'Cell', undo: bool = False) -> None:
+        p1x = self._p1.x + ((self._p2.x - self._p1.x) // 2)
+        p1y = self._p1.y + ((self._p2.y - self._p1.y) // 2)
+        p1 = Point(p1x, p1y)
+        p2x = to_cell._p1.x + ((to_cell._p2.x - to_cell._p1.x) // 2)
+        p2y = to_cell._p1.y + ((to_cell._p2.y - to_cell._p1.y) // 2)
+        p2 = Point(p2x, p2y)
+        connecting = Line(p1, p2)
+        if not undo:
+            self._win.draw_line(connecting, "red")
+        else:
+            self._win.draw_line(connecting, "gray")
+
